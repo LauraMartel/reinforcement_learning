4da1e1a2078fab43280659ca206f8d348051c0b6
laura martel <lpm.martel@gmail.com>
2022-12-14 14:53:41 +0100

    last version reinforcement learning

--- reinforcement_learning/RL_pygame_class.py
+++ reinforcement_learning/RL_pygame_class.py
@@ -12,15 +12,45 @@ import matplotlib.pyplot as plt
 class dungeon_map:
 
     # Initializing function with the constants which will be used through the class
-    def __init__(self, size, walls, learning_rate, discount, epsilon, reward_treasure, punition_walls, reward_exit):
+    def __init__(self, size, walls, learning_rate, discount,
+                 epsilon, reward_treasure, punition_walls, reward_exit):
 
         ######## --------Screen where the dungeon map will be displayed------- ########
 
         # choose the size of the maze. Here 4x4.
         self.n = size
 
+        # colors for the background
+        self.colors =  [(51,51,51) for i in range(self.n**2)]
+
+        # dispay a screen with pygame with size 4x4
+        self.screen = pygame.display.set_mode((self.n*100,self.n*100))
+
+        # background color. black here.
+        self.background = (51,51,51)
+
+        ####### --------------------Movement--------------------------##########
+        self.actions = {"up": 0,"down" : 1,"left" : 2,"right" : 3}
+
+        # initial position
+        self.current_pos = [0,0]
+
+        ######## --------Parameters for the reinforcement learning-------- ########
+        # The learning rate or step size determines to what extent
+        # newly acquired information overrides old information.
+        # ranges between 0 and 1
+        self.alpha = learning_rate
+
+        # weight measure of how important do we find future actions
+        # vs current action or future reward over current reward. values between 0 and 1
+        self.gamma = discount
+
+        # values between 0 and 1. The higher the epsilon, the more likely we are to perform a random action
+        self.epsilon = epsilon
+
         # number of walls you need
         self.penalities = walls
+
 
         # punition on the walls
         self.punition_walls = punition_walls
@@ -31,40 +61,15 @@ class dungeon_map:
         # reward to reach the exit
         self.reward_exit = reward_exit
 
-        # dispay a screen with pygame with size 4x4
-        self.screen = pygame.display.set_mode((self.n*100,self.n*100))
-
-        # background color. black here.
-        self.background = (51,51,51)
-
-        ######## --------Parameters for the reinforcement learning-------- ########
-        # The learning rate or step size determines to what extent newly acquired information overrides old information.
-        # ranges between 0 and 1
-        self.alpha = learning_rate
-
-        # weight measure of how important do we find future actions vs current action or future reward over current reward. values between 0 and 1
-        self.gamma = discount
-
-        # values between 0 and 1. The higher the epsilon, the more likely we are to perform a random action
-        self.epsilon = epsilon
-
-        ####### Movement
-        self.actions = {"up": 0,"down" : 1,"left" : 2,"right" : 3}
-
-        # initial position
-        self.current_pos = [0,0]
-
         # initial q-learning table (Q) 16x4. 16 as 4 actions up, down, left, right
         self.Q = np.zeros((self.n**2,4))
 
-        # colors for the background
-        self.colors =  [(51,51,51) for i in range(self.n**2)]
 
         # empty list to be able to do the reinforcement learning
         self.terminals = []
 
         # initial rewards matrix with only -1 size 4x4 allows to penalize the path
-        # and force the agent to go to the treasure
+        # and force the agent to go to the treasure (not stay blocked) then to the exit.
         # This can eventually be put as a user input.
         self.reward = -np.ones((self.n,self.n))
 
@@ -76,7 +81,7 @@ class dungeon_map:
     def layout(self):
         # exit, I put the reward for the exit to be the highest one. To force to go outside of treasure.
         self.reward[self.n-1,self.n-1] = self.reward_exit
-        # treasure, I put -1 for the treasure for the ball to pass by the value, but not stay there.
+        # treasure, I put 0 for the treasure for the ball to pass by the value, but not stay there.
         self.reward[2,1] = self.reward_treasure
         # color for the rectangle start
         self.colors[0] = (0,255,0)
@@ -95,11 +100,15 @@ class dungeon_map:
             if self.reward[i,j] == -1 and [i,j] != [0,0] and [i,j] != [self.n-1,self.n-1] and [i,j]!=[1,2]:
                 # punition for the walls which have the highest negative value
                 self.reward[i,j] = self.punition_walls
-                # keep at -1 for the 4 walls
+                # keep at -1 for the 4 walls. You can also modify it to change the number of walls.
                 self.penalities -= 1
                 # colors of the cases with penalties
                 self.colors[self.n*i+j] = (255,0,0)  #red
-                self.terminals.append(self.n*i+j)
+                # if we place terminals here, the agent will not explore all the maze and exclude
+                # some parts. I feel that somehow it is slower like that.
+                # As I want it to explore everything, I put the terminals in the function episode under.
+                # but you can change.
+                # self.terminals.append(self.n*i+j)
 
         # draw grid in the pygame window
         c = 0
@@ -111,12 +120,12 @@ class dungeon_map:
                 pygame.draw.rect(self.screen,self.colors[c],(j+3,i+3,j+95,i+95),0)
                 c+=1
 
-    # Function to make the agent move (pink ball)
+    # Function to make the agent move.
     def select_action(self, current_state):
         # create an empty list to store the future actions
         possible_actions = []
 
-        # if a random number is <= to epsilon you can move
+        # if a random number is <= to epsilon you can move and predict next action
         if np.random.uniform() <= self.epsilon:
             if self.current_pos[1] != 0:
                 possible_actions.append("left")
@@ -127,7 +136,7 @@ class dungeon_map:
             if self.current_pos[0] != self.n-1:
                 possible_actions.append("down")
             action = self.actions[possible_actions[r(0,len(possible_actions) - 1)]]
-        # if not, you use the Q table
+        # if not, you use the Q table to predict next action
         else:
             m = np.min(self.Q[current_state])
             #  curent state and the action (up)
@@ -171,6 +180,7 @@ class dungeon_map:
                 states[(i,j)] = k
                 k+=1
 
+        # create the initial current state
         self.current_state = states[(self.current_pos[0], self.current_pos[1])]
 
         # create the action
@@ -183,6 +193,7 @@ class dungeon_map:
             self.current_pos[1] -= 1
         elif action == 3:
             self.current_pos[1] += 1
+
         # create the new state by inserting the current position as a tuple in the randomly created state before
         new_state = states[(self.current_pos[0],self.current_pos[1])]
 
@@ -201,15 +212,16 @@ class dungeon_map:
 
 
 # call the class by defining:
-# the size of the cell, the number of walls, the learning rate, the discount, the epsilon
+# the size of the cell (here 4x4 but can be any square matrix),
+# the number of walls, the learning rate, the discount, the epsilon
 # the reward/penalty for the treasure, the walls penalty, the exit reward
 # you can change these values as you like.
-dm = dungeon_map(8, 4, 0.2, 0.9, 0.1, 0, -10, 10)
+dm = dungeon_map(4, 4, 0.2, 0.9, 0.1, 0, -10, 10)
 
 
 # make the program run in an infinite loop
-# I noticed that the 1st iteration is often not good with the start or the exit being blocked.
-# Restart a new one.
+# I noticed that the 1st iteration/epoch is often not good with the start or the exit being blocked by the walls.
+# Restart a new one in this case.
 run = True
 while run:
     sleep(0.1)
